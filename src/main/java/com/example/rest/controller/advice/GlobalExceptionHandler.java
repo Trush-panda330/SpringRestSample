e2ae70f0b2552cf9b5995b90489979b7e9adf245
uetak330 <uetak330@gmail.com>
2024-12-23 13:32:20 +0900

    未使用のインポートの削除

--- src/main/java/com/example/rest/controller/advice/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/controller/advice/GlobalExceptionHandler.java
@@ -3,14 +3,12 @@ package com.example.rest.controller.advice;
 import com.example.rest.exception.ErrorResponse;
 import com.example.rest.exception.UserNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
